604dda52de567568365a76848c8dc2c77dfa7276
Cejudo <cejudo@outlook.com>
2018-10-01 22:02:13 -0500

    Adding project for the 1.1
    Created the project where I solve the 1.1 problem (Check if a string has all unique characters)

---
+++ P1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
