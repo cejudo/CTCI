41d9499d3bcaf344c04727e860d6b267e5282fb2
Cejudo <cejudo@outlook.com>
2018-10-02 22:35:30 -0500

    1st Try
    Finished the first try

---
+++ P1.1/Try1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static bool HasEveryCharacter(string test)
+        {
+            bool[] uniqueChars = new bool[128];
+            var testChars = test.ToCharArray();
+
+            for (int i = 0; i < test.Length; i++)
+            {
+                var ascii = (int)testChars[i];
+                if (uniqueChars[ascii])
+                {
+                    return false;
+                }
+                uniqueChars[ascii] = true;
+            }
+            return true;
+        }
+    }
+}
